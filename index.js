838ccfd661b487379f0660a263ba910b62c8dc5a
jim4067 <jimmyimpulse2@gmail.com>
2020-11-23 17:09:39 +0300

    feats: connected to mongodb database, changed few type defs for graphql and refactored code

--- index.js
+++ index.js
@@ -1,89 +1,31 @@
 const { ApolloServer, UserInputError, gql } = require('apollo-server');
 const { v1: uuid, v1 } = require('uuid');
+const mongoose = require('mongoose');
 
-let authors = [
-	{
-		name: "Robert Martin",
-		id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-		born: 1952,
-	},
-	{
-		name: "Martin Fowler",
-		id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-		born: 1963,
-	},
-	{
-		name: "Fyodor Dostoevsky",
-		id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-		born: 1821,
-	},
-	{
-		name: "Joshua Kerievsky", // birthyear not known
-		id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-	},
-	{
-		name: "Sandi Metz", // birthyear not known
-		id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-	},
-]
+const Author = require('./models/author');
+const Book = require('./models/book');
+
+const connection = require('./utils/connection');
 
 /*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
+If you create a user in the admin databse  then you can use the libe below with authSource being in tha admin db
+const MONGODB_URL = `mongodb://${connection.MONGO_USERNAME}:${connection.MONGO_PASSWORD}@${connection.MONGO_HOSTNAME}:${connection.MONGO_PORT}/${connection.MONGO_DB}?authSource=admin`;
+if You create a db somewhere else then the line above should not include the authSource
 */
 
-let books = [
-	{
-		title: "Clean Code",
-		published: 2008,
-		author: "Robert Martin",
-		id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring"]
-	},
-	{
-		title: "Agile software development",
-		published: 2002,
-		author: "Robert Martin",
-		id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-		genres: ["agile", "patterns", "design"]
-	},
-	{
-		title: "Refactoring, edition 2",
-		published: 2018,
-		author: "Martin Fowler",
-		id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring"]
-	},
-	{
-		title: "Refactoring to patterns",
-		published: 2008,
-		author: "Joshua Kerievsky",
-		id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring", "patterns"]
-	},
-	{
-		title: "Practical Object- Oriented Design, An Agile Primer Using Ruby",
-		published: 2012,
-		author: "Sandi Metz",
-		id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring", "design"]
-	},
-	{
-		title: "Crime and punishment",
-		published: 1866,
-		author: "Fyodor Dostoevsky",
-		id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-		genres: ["classic", "crime"]
-	},
-	{
-		title: "The Demon",
-		published: 1872,
-		author: "Fyodor Dostoevsky",
-		id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-		genres: ["classic", "revolution"]
-	},
-];
+const MONGODB_URL = `mongodb://${connection.MONGO_USERNAME}:${connection.MONGO_PASSWORD}@${connection.MONGO_HOSTNAME}:${connection.MONGO_PORT}/${connection.MONGO_DB}`;
 
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+	.then(() => {
+		console.log("connected to mongDB");
+	})
+	.catch((error) => {
+		console.log("aaaw shit. Here we go again -> ", error.message);
+		console.log(MONGODB_URL);
+	});
+
+
+//graphql type definations
 const typeDefs = gql`
   type Author {
 	  name: String!
@@ -94,8 +36,8 @@ const typeDefs = gql`
 
   type Book {
 	  title: String!
-	  published: String!
-	  author: String!
+	  published: Int!
+	  author: Author!
 	  id: ID!
 	  genres: [String!]!
   }
@@ -110,31 +52,31 @@ const typeDefs = gql`
 	  addBook(
 		  title: String!
 		  author: String!
-		  published: String!
+		  published: Int!
 		  genres: [String!]!
 	  ) : Book
 	  editAuthor(
 		  name: String!
-		  setBornTo: String!
+		  setBornTo: Int!
 	  ) : Author
   }
 `;
 
 const resolvers = {
 	Query: {
-		authorCount: () => authors.length,
-		bookCount: () => books.length,
+		authorCount: () => Author.collection.countDocuments(),
+		bookCount: () => Book.collection.countDocuments(),
 		allBooks: (root, args) => {
 			if (args.author) {
-				return books.filter((book) => book.author === args.author);
+				// return books.filter((book) => book.author === args.author);
 			}
 			if (args.genre) {
-				return books.filter((book) => book.genres.find((b) => b === args.genre));
+				// return books.filter((book) => book.genres.find((b) => b === args.genre));
 			}
 			if (args.author && args.genre) {
-				return books.filter((book) => book.author === args.author && book.genres.find((el) => el === args.genre));
+				// return books.filter((book) => book.author === args.author && book.genres.find((el) => el === args.genre));
 			} else {
-				return books;
+				return Book.find({});
 			}
 		},
 		allAuthors: () => authors,
@@ -184,6 +126,6 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-	console.log(`Server ready at ${url}`)
+	console.log(`graphql server ready at ->  ${url}`)
 });
 
