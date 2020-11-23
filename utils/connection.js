d11528412c27db97f74c3a336d583ce38b02df6a
jim4067 <jimmyimpulse2@gmail.com>
2020-11-23 16:35:24 +0300

    feat: the file for connecting to the mongodb database

---
+++ utils/connection.js
@@ -0,0 +1,9 @@
+const connection = {
+    MONGO_USERNAME: "jim4067",
+    MONGO_PASSWORD: "4067",
+    MONGO_HOSTNAME: "127.0.0.1",
+    MONGO_PORT: "27017",
+    MONGO_DB: "graphql-library",
+}
+
+module.exports = connection;
